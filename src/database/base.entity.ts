00889498a74e9cba8058e4ab08466ae5e127332b
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-05-13 17:53:54 +0200

    base entity update

--- src/database/base.entity.ts
+++ src/database/base.entity.ts
@@ -3,20 +3,14 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { Exclude } from 'class-transformer';
 
 export class BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @CreateDateColumn({ type: 'timestamp' })
-  @Exclude()
   createdAt: string;
 
   @UpdateDateColumn({ type: 'timestamp' })
   updatedAt: string;
-
-  constructor(partial: Partial<BaseEntity>) {
-    Object.assign(this, partial);
-  }
 }
